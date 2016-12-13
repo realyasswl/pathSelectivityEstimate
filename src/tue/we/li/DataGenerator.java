2db8f822383b986cfb84e4bdc2b6a57ce292868e
TUE\s152022 <l.wang.3@student.tue.nl>
2016-12-13 14:14:15 +0100

    class DataGenerator created

---
+++ src/tue/we/li/DataGenerator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tue.we.li;
+
+/**
+ *
+ * @author s152022
+ */
+public class DataGenerator {
+    
+}
